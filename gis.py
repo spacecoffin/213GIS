4ef54bd35fb105fb5f43bdd6c3bf8a90ffef53ec
spacecoffin <reed.rosenberg@gmail.com>
2015-05-06 14:21:33 -0400

    Created gis class file
    Also added script for working through NetworkX tutorial.

---
+++ gis.py
@@ -0,0 +1,12 @@
+import networkx as nx
+
+class gis:
+    # storing all of the information in appropriate data structures, and then
+    # answering queries efficiently
+    def __init__(self):
+        return()
+    
+    def graphnew():
+        G=nx.Graph()
+    
+    
---
+++ nx_tutorial.py
@@ -0,0 +1,15 @@
+import networkx as nx
+
+# Create an empty graph with no nodes or edges
+G = nx.graph()
+
+# Add one node
+G.add_node(1)
+
+# Add a list of nodes
+G.add_nodes_from([2,3])
+
+# Or add any nbunch of nodes
+# Nbunch = iterable container of nodes that is not itself a node in the graph (e.g. a list, set, graph, file, etc.)
+H=nx.path_graph(10)
+G.add_nodes_from(H)
