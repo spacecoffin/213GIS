64034ab14cf8f8bec7ea7d43bedc74143335a9e1
spacecoffin <reed.rosenberg@gmail.com>
2015-05-07 14:29:49 -0400

    Added code to skip the comment lines of gis.dat (that begin with '*')

--- gisparse.py
+++ gisparse.py
@@ -1,5 +1,5 @@
 import os
-import re
+#import re
 
 if __name__ == '__main__':
     gisdat = 'gis.dat'
@@ -13,8 +13,55 @@ if __name__ == '__main__':
     # Solution: If line begins with '*' char, go to the next line without
     # storing any data.
 
+    line = [w.rstrip('\n') for w in file.readlines()]
+
+
+    while True:
+        line = file.readline().rstrip('\n')
+        if not line:    # check for EOF
+            break
+        if line[1] is '*':
+            continue
+        else:
+            print(line)
+
+    file.close()
+
+    """
+    wordch = []
+    prevNonWhite = None
+    period = '.'
+    apostrophe = "'"
+    while True:
+        chr = file.read(1)
+        if not chr:               # check if end-of-file
+            if len(wordch) >= 2 and (wordch[0].islower() or prevNonWhite != '.'):
+                addToWordList(''.join(wordch))
+            break
+        elif chr.isalpha():
+            wordch.append(chr)
+        elif chr is apostrophe and (len(wordch) >= 2 or wordch[0] == "I"):
+            wordch.append(chr)
+        elif chr is apostrophe:
+            prevNonWhite = apostrophe
+        elif prevNonWhite is apostrophe:
+            wordch = []
+            prevNonWhite = period if chr is period else None
+        elif len(wordch) >= 2 and (wordch[0].islower() or prevNonWhite is period):
+            addToWordList(''.join(wordch))
+            wordch = []
+            prevNonWhite = period if chr is period else None
+        else:
+            wordch = []
+            prevNonWhite = period if chr is period else None
+        finally:
+            file.close()
+    """
+
+
+
+
     # self.words = [w.rstrip('\n') for w in file.readlines()]
     # self.mapper = {}
     # self.keepwords = []
 
-    file.close()
