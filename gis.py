2eae88bee0263c373d6c06ffd24e4fc75d7b1d53
spacecoffin <reed.rosenberg@gmail.com>
2015-05-13 13:05:40 -0400

    Added several methods.

--- gis.py
+++ gis.py
@@ -1,25 +1,53 @@
-import networkx as nx
 import os
+import networkx as nx
+from decimal import Decimal, getcontext
+
 
 class Gis:
     # Gis reads gis.dat, stores all of the information in appropriate data
     # structures, and then answers queries efficiently.
-    def __init__(self, gisdat='gis.dat'):
+    def __init__(self):
         # check if the file exists
-        assert os.path.exists(gisdat), 'City data file \'%s\' does not ' \
-                                       'exist.' % gisdat
-        file = open(gisdat, 'r')
-        self.words = [w.rstrip('\n') for w in file.readlines()]
-        self.mapper = {}
-        self.keepwords = []
+        assert os.path.exists('gis.dat'), 'gis.dat does not exist.'
 
-        file.close()
+        # Create an empty graph with no nodes or edges
+        self.G = nx.Graph()
+        citylist = []
+        getcontext().prec = 5
 
-    def selectCities(attribute, lowerBound, upperBound):
-        # This method will be used to “select” a set of cities that satisfy
+        with open('gis.dat') as gisf:
+            getcontext().prec = 5
+            for line in gisf.readlines():
+                if line.startswith('*'):
+                    continue
+                line.strip()
+                if line[0].isalpha():
+                    (citystate, citydata) = line.split('[')
+                    (city, state) = citystate.split(', ')
+                    (latlon, pop) = citydata.split(']')
+                    (lat, lon) = latlon.split(',')
+                    G.add_node(city, name=city, state=state,
+                               lat=float(Decimal(lat) * Decimal(0.01)),
+                               lon=float(Decimal(lon) * Decimal(0.01)),
+                               pop=int(pop.rstrip()))
+                    citylist.insert(0, city)
+                if line[0].isdigit():
+                    i=0
+                    distlist = line.split()
+                    for dist in distlist:
+                        int(dist)
+                        G.add_edge(city, citylist[i], weight=dist)
+                        i=i+1
+
+        self.H = self.G
+        self.allcities = self.G.nodes()
+        self.alledges = self.G.edges()
+
+    def selectCities(self, attribute, lowerBound, upperBound):
+        # This method will be used to "select" a set of cities that satisfy
         # some conditions.
-        # The argument attribute is a string that can be one of “name”,
-        # “state”, “latitude”, “longitude”, and “population.”
+        # The argument attribute is a string that can be one of "name",
+        # "state", "latitude", "longitude", and "population."
         # These examples should indicate to you that while attribute is
         # guaranteed to be a string, lowerBound and upperBound will have
         # different types, depending on the value of attribute.
@@ -28,36 +56,76 @@ class Gis:
         # allows us to use selectCities repeatedly to select a set of cities
         # that satisfy several constraints.
 
-        return()
+        if attribute is 'name':
+            self.H = self.H.subgraph([c for c, a in self.H.node.items() if
+                            ord((a['name'])[0]) >= ord(lowerBound[0])
+                            and
+                            ord((a['name'])[0]) <= ord(upperBound[0])])
+        elif attribute is 'state':
+            self.H = self.H.subgraph([c for c, a in self.H.node.items() if
+                            ord((a['state'])[0]) >= ord(lowerBound[0])
+                            and
+                            ord((a['state'])[0]) <= ord(upperBound[0])])
+        elif attribute is 'latitude':
+            self.H = self.H.subgraph([c for c, a in self.H.node.items(
+            ) if a['lat'] >= lowerBound and a['lat'] <= upperBound])
+        elif attribute is 'longitude':
+            self.H = self.H.subgraph([c for c, a in self.H.node.items(
+            ) if a['lon'] >= lowerBound and a['lon'] <= upperBound])
+        elif attribute is 'population':
+           self.H = self.H.subgraph([c for c, a in self.H.node.items(
+            ) if a['pop'] >= lowerBound and a['pop'] <= upperBound])
+        else:
+            print('"{}" is not a valid attribute.\nPlease enter one of '
+                  '"name", "state", "latitude", "longitude" or '
+                  '"population".'.format(attribute))
 
-    def selectAllCities(self): # self?
-        # These methods respectively select and un-select all cities.
+        return ()
 
-        return()
+    def selectAllCities(self):
+        # Select all cities.
+        # The allcities list created during initialization is iterated
+        # through rather than iterating through calls to G as a matter of
+        # computational efficiency.
+        self.H.add_nodes_from(self.allcities)
+        return ()
 
-    def unselectAllCities(self): # self?
-        # These methods respectively select and un-select all cities.
+    def unselectAllCities(self):
+        # Un-select all cities.
+        # Create a list of all cities in the graph which is then iterated
+        # through for removal. This ensures that only currently selected
+        # cities are iterated through, not all cities (a trade off of
+        # computational efficiency for spatial efficiency).
+        nlist = self.H.nodes()
+        self.H.remove_nodes_from(nlist)
+        return ()
 
-        return()
-
-    def selectEdges(lowerBound, upperBound):
+    def selectEdges(self, lowerBound, upperBound):
         # Here lowerBound and upperBound specify a "distance range."
         # For example, if lowerBound is set to 0 and upperBound is set to
         # 500, this method will select all edges between pairs of cities
         # whose distance (as specified in gis.dat) is at most 500 miles.
         # Assume that initially no edges are selected.
 
-        return()
+        return ()
 
-    def selectAllEdges(self): # self?
-        # These methods respectively select and un-select all edges.
+    def selectAllEdges(self):
+        # Select all edges.
+        # The alledges list created during initialization is iterated
+        # through rather than iterating through calls to G as a matter of
+        # computational efficiency.
+        self.H.add_edges_from(self.alledges)
+        return ()
 
-        return()
-
-    def unselectAllEdges(self): # self?
-        # These methods respectively select and un-select all edges.
-
-        return()
+    def unselectAllEdges(self):
+        # Un-select all edges.
+        # Create a list of all edges in the graph which is then iterated
+        # through for removal. This ensures that only currently selected
+        # edges are iterated through, not all edges (a trade off of
+        # computational efficiency for spatial efficiency).
+        elist = self.H.edges()
+        self.H.remove_edges_from(elist)
+        return ()
 
     def makeGraph(self):
         # This method makes and returns a graph whose vertex set is the set of
@@ -65,13 +133,13 @@ class Gis:
         # pairs of selected cities.
         # Using algorithms studied in class, we can find out if in such a
         # graph it is possible to travel from any "high population" city to
-        # any other “high population” city, without visiting any "low
+        # any other "high population" city, without visiting any "low
         # population" city and furthermore using only hops of length at most
         # 200 miles.
 
-        return()
+        return ()
 
-    def printCities(attribute, choice):
+    def printCities(self, attribute, choice):
         # As before, attribute can be one of "name", "state", "latitude",
         # "longitude", and "population." This methods prints all selected
         # cities sorted in increasing order by the given attribute.
@@ -80,9 +148,9 @@ class Gis:
         # The second parameter choice ('S' or 'F') determines whether the
         # requested output should be displayed in "short" form or "full" form.
 
-        return()
+        return ()
 
     def printEdges(self):
         # This should print all selected edges, in no particular order.
 
-        return()
+        return ()
